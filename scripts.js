caa0df6bd244f879fff63e36d08ac18effbbaa9a
danielbahia <web.danielbahia@gmail.com>
2021-06-25 20:02:37 -0300

    Adicionado a função que dá game over quando a cobrinha se choca com o proprio corpo

--- scripts.js
+++ scripts.js
@@ -40,11 +40,18 @@ function update(event){
 }
 
 function iniciarJogo(){
-
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game Over! Você perde, he he he!")
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -55,7 +62,7 @@ function iniciarJogo(){
 
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
-    if(direction == "up") snakeY -= box;
+    if(direction == "up")   snakeY -= box;
     if(direction == "down") snakeY += box;
 
     if(snakeX != food.x || snakeY != food.y){
@@ -64,8 +71,6 @@ function iniciarJogo(){
         food.x = Math.floor(Math.random() * 15 + 1) * box,
         food.y = Math.floor(Math.random() * 15 + 1) * box
     }
-    
-
 
     let newHead = {
         x: snakeX,
@@ -76,4 +81,3 @@ function iniciarJogo(){
 }
 
 let jogo = setInterval(iniciarJogo, 100);
-
